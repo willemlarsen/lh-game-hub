02a0f719ab92991ad129d3b26f3b0a57982b9067
Willem Larsen <willemlarsen@gmail.com>
2014-09-30 10:32:22 -0700

    License is in own file

--- client/src/app/home/home.spec.js
+++ client/src/app/home/home.spec.js
@@ -12,3 +12,52 @@ describe( 'home section', function() {
   }));
 });
 
+"use strict";
+
+describe('HomeController', function () {
+  var  scope,
+       home = ["home"];
+
+    beforeEach(function () {
+        module("app");
+
+        inject(function ($rootScope, $controller, $firebase) {
+            scope = $rootScope.$new();
+
+            mockFirebase = sinon.stub($firebase);
+            // mockCatalogRepository.getCatalog.returns(catalog);
+            // mockRegistrationService = sinon.stub(registrationService);
+
+            $controller("HomeController", {
+                $scope: scope,
+                firebase: mockFirebase
+            });
+
+        });
+    });
+
+    describe('when the controller first loads', function () {
+
+        it('the course catalog is retrieved', function () {
+            sinon.assert.calledOnce(mockCatalogRepository.getCatalog);
+        });
+        it('puts the catalog on the scope', function() {
+            expect(scope.catalog).toEqual(catalog);
+        });
+
+    });
+    describe('when registering for a course', function() {
+        var courseId = 'courseId';
+        var response = {success: true, message: ''};
+
+        it('adds the course to the wizard\'s schedule', function() {
+            mockRegistrationService.register.returns(response);
+            scope.register(courseId);
+            sinon.assert.calledWith(mockRegistrationService.register, courseId);
+        });
+
+    });
+
+});
+
+
