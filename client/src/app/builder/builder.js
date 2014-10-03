599c9d7fdb1ef8a01f008e6755574b6445ec796c
Willem Larsen <willemlarsen@gmail.com>
2014-10-03 10:05:20 -0700

    add fields to exchanges form

--- client/src/app/builder/builder.js
+++ client/src/app/builder/builder.js
@@ -1,31 +1,33 @@
-angular.module( 'app.builder', [
-  'ui.router',
-  'firebase'
+angular.module('app.builder', [
+ 'ui.router',
+ 'firebase'
 ])
 
-.config(function config( $stateProvider ) {
-  $stateProvider.state( 'builder', {
-    url: '/builder',
-    views: {
-      "main": {
-        controller: 'BuilderCtrl',
-        templateUrl: 'builder/builder.tpl.html'
-      }
-    },
-    data:{ pageTitle: 'Builder' }
-  });
+.config(function config($stateProvider) {
+ $stateProvider.state('builder', {
+  url: '/builder',
+  views: {
+   "main": {
+    controller: 'BuilderCtrl',
+    templateUrl: 'builder/builder.tpl.html'
+   }
+  },
+  data: {
+   pageTitle: 'Builder'
+  }
+ });
 })
 
-.controller( 'BuilderCtrl', function BuilderController( $scope, $firebase ) {
-    $scope.exchanges = [];
-    $scope.forms = [{}];
-    $scope.addFields = function (form) {
-      if(typeof form.exchanges === 'undefined') {
-        form.exchanges = [];
-      }
-      form.exchanges.push({name:'', ac: '', a_number: '', p_id: '' });
-    };
+.controller('BuilderCtrl', function BuilderController($scope, $firebase) {
+  $scope.exchanges = [
+   ["What is that?", "That is a cup."]
+  ];
 
+  $scope.form = { 'exchanges': [ [] ] };
+
+  $scope.addFields = function(form) {
+    form.exchanges.push([]);
+  };
 })
 
 ;
---
+++ client/src/app/builder/builder.spec.js
@@ -0,0 +1,42 @@
+describe('BuilderCtrl', function() {
+ var scope,
+  builder = ["builder"];
+
+ beforeEach(function() {
+  module("app");
+
+  inject(function($rootScope, $controller) {
+   scope = $rootScope.$new();
+
+   // mockFirebase = sinon.stub($firebase);
+
+   $controller("BuilderCtrl", {
+    $scope: scope
+   });
+
+  });
+
+ });
+
+ describe('when the controller first loads', function() {
+
+  // it('the game is retrieved', function () {
+  //     sinon.assert.calledOnce(mockFirebase.getGame);
+  // });
+  // it('puts the catalog on the scope', function() {
+  //     expect(scope.catalog).toEqual(catalog);
+  // });
+
+ });
+
+ describe('exchanges form', function() {
+
+  it('appends paired question and answer fields to display', function() {
+    var form = { 'exchanges': [ [] ] };
+    scope.addFields(form);
+    expect(form.exchanges.length).toBe(2);
+  });
+
+ });
+
+});
