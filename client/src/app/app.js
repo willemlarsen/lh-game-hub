58475bb4c23ca78bb4e2890e92d419a1bf6b8b26
Willem Larsen <willemlarsen@gmail.com>
2014-10-09 10:27:28 -0700

    UI changes

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -17,7 +17,7 @@ angular.module( 'app', [
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | app' ;
+      $scope.pageTitle = toState.data.pageTitle + ' | Hub' ;
     }
   });
 })
