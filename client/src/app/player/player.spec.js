cdf94072a4a986a5abe90da2689c67b8ad8426f3
zhon <zhon@xputah.org>
2014-10-14 10:51:26 -0600

    Formatted code

--- client/src/app/player/player.spec.js
+++ client/src/app/player/player.spec.js
@@ -1,43 +1,44 @@
 
 describe('PlayerCtrl', function () {
   var   scope,
-        mockSquareRepository,
-        player = ["player"];
+  mockSquareRepository,
+  player = ["player"];
 
-    beforeEach(function () {
-        module("app");
+  beforeEach(function () {
+    module("app");
 
-        inject(function ($rootScope, $controller, SquareRepository) {
-            scope = $rootScope.$new();
+    inject(function ($rootScope, $controller, SquareRepository) {
+      scope = $rootScope.$new();
 
-            mockSquareRepository = sinon.stub(SquareRepository);
+      mockSquareRepository = sinon.stub(SquareRepository);
 
-            $controller("PlayerCtrl", {
-                $scope: scope
-            });
-
-        });
-    });
-
-    describe('when the game player page is loaded', function () {
-        it('loads the first square', function () {
-            sinon.assert.calledOnce(mockSquareRepository.getSquare);
-        });
-
-        it('loads all squares for the first lap', function () {
-            // iterates through all the squares in SquareRepository
-            // and displays them in player.
-        });
-    });
-
-    describe('audio_path', function() {
-
-        // it('creates the path', function() {
-        //   expect($scope.audio_path("onething", "twothing")).toBe("onethingtwothing");
-
-        // });
+      $controller("PlayerCtrl", {
+        $scope: scope
+      });
 
     });
+  });
+
+  describe('when the game player page is loaded', function () {
+
+    it('loads the first square', function () {
+      sinon.assert.calledOnce(mockSquareRepository.getSquare);
+    });
+
+    it('loads all squares for the first lap', function () {
+      // iterates through all the squares in SquareRepository
+      // and displays them in player.
+    });
+  });
+
+  describe('audio_path', function() {
+
+    // it('creates the path', function() {
+    //   expect($scope.audio_path("onething", "twothing")).toBe("onethingtwothing");
+
+    // });
+
+  });
 
 });
 
