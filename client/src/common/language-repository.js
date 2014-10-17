977036456216f5fa512d40f554d48e5eb1ed45a6
zhon <zhon@xputah.org>
2014-10-17 15:28:01 -0600

    Formatted code

--- client/src/common/language-repository.js
+++ client/src/common/language-repository.js
@@ -1,31 +1,31 @@
 
 angular.module('app').factory('LanguageRepository', function(session, $q, $firebase) {
 
-    var ref = new Firebase("https://languagehuntgamehub.firebaseio.com/");
+  var ref = new Firebase("https://languagehuntgamehub.firebaseio.com/");
 
-    var retrieveChild = function(child) {
-        var deferred = $q.defer();
-        var languageRef = ref.child(child);
-        languageRef.once('value', function(data) {
-          deferred.resolve( data.val() );
-        });
-        return deferred.promise;
-    };
+  var retrieveChild = function(child) {
+    var deferred = $q.defer();
+    var languageRef = ref.child(child);
+    languageRef.once('value', function(data) {
+      deferred.resolve( data.val() );
+    });
+    return deferred.promise;
+  };
 
-    return {
+  return {
 
-      save: function() {
-      },
+    save: function() {
+    },
 
-      getLanguages: function() {
-        return retrieveChild('languages');
-      },
+    getLanguages: function() {
+      return retrieveChild('languages');
+    },
 
-      getLanguage: function (language) {
-        return retrieveChild(language);
-      }
+    getLanguage: function (language) {
+      return retrieveChild(language);
+    }
 
- };
+  };
 
 });
 
