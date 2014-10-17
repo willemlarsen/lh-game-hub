bd6296a9165f1f84923a47dfcb8852f89444e438
zhon <zhon@xputah.org>
2014-10-16 20:21:26 -0600

    Get promise tests passing

--- client/src/app/language/language.spec.js
+++ client/src/app/language/language.spec.js
@@ -1,19 +1,22 @@
 
-describe('language', function() {
- var scope,
+describe('LanguageCtrl', function() {
+  var scope,
   language,
+  deferredLanguages,
   mockLanguageRepository;
 
  beforeEach(function() {
   module("app");
 
-  inject(function($rootScope, $controller, LanguageRepository) {
+  inject(function($rootScope, $controller, LanguageRepository, $q) {
 
     language = ["language"];
 
     scope = $rootScope.$new();
 
     mockLanguageRepository = sinon.stub(LanguageRepository);
+    deferredLanguages = $q.defer();
+    mockLanguageRepository.getLanguages.returns(deferredLanguages.promise);
 
     $controller("LanguageCtrl", {
       $scope: scope,
@@ -25,16 +28,20 @@ describe('language', function() {
  });
 
  describe('when the controller first loads', function() {
+   var languages = [ 'Engrish', 'Spanglish' ];
 
-  xit ('gets all the languages options', function() {
-    sinon.assert.calledOnce(mockLanguageRepository.getLanguages);
-  });
+   beforeEach(function() {
+     deferredLanguages.resolve(languages);
+     scope.$apply();
+   });
 
-  xit ('languages are loaded into scope', function() {
-    var languages = [ 'Engrish', 'Spanglish' ];
-    mockLanguageRepository.getLanguages.returns(languages);
-    expect(scope.languages).toBe(languages);
-  });
+   it ('gets all the languages options', function() {
+     sinon.assert.calledOnce(mockLanguageRepository.getLanguages);
+   });
+
+   it ('languages are loaded into scope', function() {
+     expect(scope.languages).toBe(languages);
+   });
 
  });
 
